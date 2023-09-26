2058e37b510634530e7604451b01fc3377c23f13
michael <michaelngige@gmail.com>
2023-09-26 14:29:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(param = "roller-skate") {
+    return `This Saturday, I want to ${param}!`;
+  }
+  
+  saturdayFun(); 
+  saturdayFun("bathe my dog!"); 
+
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  mondayWork();
+  mondayWork("work from home.")
+
+
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated developer"))  
